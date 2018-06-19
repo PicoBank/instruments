6a940b6c801cdae1e0e869085789e23fb004bb96
Jacques Desmazières <jacques.desmazieresgmail.com>
2018-06-19 21:02:59 +0200

    chargement pgloader en mode etl

---
+++ database/migrations/00005_trivial_instrument_etl.sql
@@ -0,0 +1,43 @@
+-- +goose Up
+
+CREATE TABLE etl_bats_instrument (
+    company_name                                    VARCHAR(128),
+    bats_name                                       VARCHAR(12),
+    isin					                        VARCHAR(12),
+    currency				                        VARCHAR(3),
+    mic						                        VARCHAR(4),
+    reuters_exchange_code	                        VARCHAR(4),
+    lis_local				                        DECIMAL(21,9),
+    live					                        VARCHAR(1),
+    tick_type				                        VARCHAR(12),
+    reference_price			                        DECIMAL(21,9),
+    bats_prev_close			                        DECIMAL(21,9),
+    live_date				                        DATE,
+    bloomberg_primary		                        VARCHAR(32),
+    bloomberg_bats			                        VARCHAR(32),
+    mifid_share				                        VARCHAR(1),
+    asset_class				                        VARCHAR(8),
+    matching_unit			                        NUMERIC(2),
+    euroccp_enabled			                        BOOLEAN,
+    xclr_enabled			                        BOOLEAN,
+    lchl_enabled			                        BOOLEAN,
+    reuters_ric_primary		                        VARCHAR(99),
+    reuters_ric_bats		                        VARCHAR(99),
+    reference_adt_eur		                        DECIMAL(21,9),
+    csd						                        VARCHAR(16),
+    corporate_action_status	                        VARCHAR(99),
+    supported_services		                        VARCHAR(8),
+    trading_segment			                        VARCHAR(4),
+    printed_name						            VARCHAR(8),
+    periodic_auction_max_duration			        NUMERIC(8),
+    periodic_auction_min_order_entry_size	        NUMERIC(8),
+    periodic_auction_min_order_entry_notional		NUMERIC(8),
+    max_otr_count						            NUMERIC(8),
+    max_otr_volume						            NUMERIC(12),
+    capped						                    NUMERIC(2),
+    venue_cap_percentage						    NUMERIC(8),
+    venue_uncap_date                                DATE
+);
+
+-- +goose Down
+DROP TABLE etl_bats_instrument;
