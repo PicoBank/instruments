6e3b2da1cd61a053aabdcfed97ff41d071b9a7e0
Dominique Broeglin <dominique.broeglin@gmail.com>
2018-06-24 14:11:09 +0200

    Added loading of currency (src: Banque de France), BATS ETL done by pgloader

---
+++ database/pgloader/etl_bats_instrument.sql
@@ -0,0 +1,41 @@
+
+DO $$ 
+DECLARE 
+ expected_rows integer := 49998;
+ C integer := 0;
+
+ actual_rows   record;
+BEGIN 
+    INSERT INTO instrument (
+        symbol, 
+        name, 
+        description, 
+        currency_id, 
+        instrument_class_id, 
+        from_date, thru_date, 
+        created_at, created_by, 
+        updated_at, updated_by
+    ) SELECT 
+        isin,
+        bats_name,
+        company_name,
+        instrument.id,
+        3,
+        '2001-01-01', '2999-01-01', 
+        current_timestamp, 'ETL', 
+        current_timestamp, 'ETL' 
+    FROM etl_bats_instrument JOIN instrument ON (
+        upper(etl_bats_instrument.currency) = instrument.symbol 
+        AND instrument.instrument_class_id = 1)
+    ;
+
+    GET DIAGNOSTICS c = ROW_COUNT;
+
+    RAISE INFO 'INSERT ROW COUNT %', c ;
+
+    SELECT COUNT(id) INTO actual_rows FROM instrument;
+   
+    --ASSERT actual_rows.count = expected_rows,
+    --    'Expect number of rows in instrument to be ' || expected_rows || ' not ' || actual_rows;
+END $$;
+
