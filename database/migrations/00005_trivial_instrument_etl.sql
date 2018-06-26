c192acf0618777a251a7a5fb82dc6ab52e0f113b
Jacques Desmazières <jacques.desmazieresgmail.com>
2018-06-26 21:23:39 +0200

    etl down

--- database/migrations/00005_trivial_instrument_etl.sql
+++ database/migrations/00005_trivial_instrument_etl.sql
@@ -123,3 +123,8 @@ CREATE TABLE etl.etl_csi_cash (
 
 -- +goose Down
 DROP TABLE etl.etl_bats_instrument;
+DROP TABLE etl.etl_csi_stock;
+DROP TABLE etl.etl_csi_stockoption;
+DROP TABLE etl.etl_csi_commodity;
+DROP TABLE etl.etl_csi_commodityoption;
+DROP TABLE etl.etl_csi_cash;
